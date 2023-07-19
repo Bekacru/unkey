e76909f5156690b8603898a23e29d6200316d17b
Bereket Engida <bekacru@gmail.com>
2023-07-19 19:52:34 +0300

    fix: ignore ts errors on build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
